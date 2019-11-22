3465dabbc86cd6fbe6977da70bb3d89f9fef6f8a
MkSavin@mail.ru <mksavin@mail.ru>
2019-11-22 22:10:43 +0300

    Html popup update #2

--- public/html/js/index.js
+++ public/html/js/index.js
@@ -1,4 +1,30 @@
+var updateSelects;
+(updateSelects = function(){
+    var selects = $('main select, #popup select');
+    selects.selectpicker({
+        noneSelectedText: "Ничего не выбрано",
+        countSelectedText: "Элементов выбрано: {0}",
+        noneResultsText: "Нет результатов по запросу {0}"
+    });
+})();
+
+var refreshSelects = function(){
+    var selects = $('main select, #popup select');
+    selects.selectpicker({
+        noneSelectedText: "Ничего не выбрано",
+        countSelectedText: "Элементов выбрано: {0}",
+        noneResultsText: "Не найдено: {0}"
+    }, 'refresh');
+};
+
+var updateDatepicks;
+(updateDatepicks = function(){
+    $('.jq-datepicker').datepicker({position: "top center"});
+})();
+
 $(function(){
+    refreshSelects();
+
     /* Drop Down close */
     $(document).on('click', function(e){
         var v = $(e.target);
@@ -16,12 +42,25 @@ $(function(){
 
     $(document).on('click',  '.overlay-close', function(){
         $('#popup').addClass('hidden');
+        $('.popup-toggle').removeClass('active');
+        $('body').removeClass('nooverflow');
     });
 
     $(document).on('click',  '.popup-toggle', function(){
         $("#popup").html("");
         $('.popup_stack ' + $(this).attr('popup-target')).clone().appendTo("#popup");
         $("#popup").removeClass("hidden");
-        $(this).toggleClass('active');
+        $(this).addClass('active');
+        $('body').addClass('nooverflow');
+        
+        refreshSelects();
+        updateDatepicks();
     });
+
+    $(document).on('click', 'input[type=reset]', function(){
+        setTimeout(() => {
+            $('form select').selectpicker('refresh');
+        }, 10);
+    });
+
 });
